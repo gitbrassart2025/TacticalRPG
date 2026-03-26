c0898a6fdef7bb0adea56c536906e63ddecbe37a
Hugo Boulhaut <boulhaut.hugo@gmail.com>
2026-03-26 09:41:53 +0100

    Error Debug

--- Assets/Script_Hugo/Grid_System/S_GridManager.cs
+++ Assets/Script_Hugo/Grid_System/S_GridManager.cs
@@ -11,21 +11,34 @@ public class S_GridManager : MonoBehaviour
     public Material lightMaterial; 
     public Material darkMaterial; 
 
-    private Tile[,] map;
+    private S_Tile[,] map;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        map = new Tile[width, height];
-
+        map = new S_Tile[width, height];
+        GeneratedGrid();
     }
 
     public void GeneratedGrid() {
-        for(int x = 0; x < width; x++){
-            for(int y = 0; y = height; y++) {
-                Vector3 tilePosition = new Vector3(x - width / 2, 0, y - height / 2);
 
+        for(int x = 0; x < width; x++){
+
+            for(int y = 0; y = height; y++) {
+
+                Vector3 tilePosition = new Vector3(x - width / 2, 0, y - height / 2);
+                GameObject tile = Instantiate(tilePrefab, tilePosition, Quaternion.identity);
+                tile.name = $"S_Tile {x} {y}";
+                tile.transform.SetParent(transform);
+
+                Renderer renderer = tile.GetComponent<Renderer>();
+
+                renderer.material = new Material((x + y) % 2 == 0 ? lightMaterial : darkMaterial);
+
+                tileScript.gridPosition = new Vectro2Int(x , y);
+                //Nike ta mère la pute = 1;   
+                map[x , y] = tileScript; 
             }
         }
     }
